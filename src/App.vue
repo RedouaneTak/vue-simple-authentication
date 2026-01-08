8251d37e92511cf557963b1b7681a496d0186fef
Redouane <redouane.takdjerad@gmail.com>
2026-01-08 21:39:27 +0100

    BaseInput component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import BaseInput from './components/commons/BaseInput.vue';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+ <BaseInput label="Firstname" type="text" id="firstname-input"/>
 </template>
 
 <style scoped></style>
--- src/components/commons/BaseInput.vue
+++ src/components/commons/BaseInput.vue
@@ -1,7 +1,53 @@
 <script setup lang="ts">
 
+const props = defineProps<{
+
+  label: string
+  id: string
+  type: string
+}>()
+
+const model = defineModel()
 </script>
 
 <template>
-  <input>
+
+    <label class="base-input-label" :for="props.id">
+      <span class="base-input-text">{{ props.label }}</span>
+      <input class="base-input-field" :type="props.type" v-model="model" :id="props.id" />
+    </label>
+
 </template>
+
+<style scoped>
+
+.base-input-label {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  margin-bottom: 1rem;
+  font-family: inherit;
+}
+
+.base-input-text {
+  margin-bottom: 0.25rem;
+  font-weight: 500;
+  font-size: 0.95rem;
+}
+
+.base-input-field {
+  width: 100%;
+  padding: 0.5rem 0.75rem;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  box-sizing: border-box;
+  font-size: 1rem;
+  transition: border-color 0.2s;
+}
+
+.base-input-field:focus {
+  outline: none;
+  border-color: #4f46e5;
+}
+</style>

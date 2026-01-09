2e5d2b75f7afcf34c4d269d2f2f865f249686bbc
Redouane <redouane.takdjerad@gmail.com>
2026-01-09 20:25:08 +0100

    BaseHeader component

---
+++ src/components/commons/BaseHeader.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="logo">
+      <RouterLink to="/home">My app</RouterLink>
+    </div>
+
+    <nav>
+      <RouterLink to="/register">Register</RouterLink>
+      <RouterLink to="/login">Log in</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 30px;
+  background-color: #222;
+}
+
+.logo a {
+  color: white;
+  font-size: 22px;
+  font-weight: bold;
+  text-decoration: none;
+}
+
+nav {
+  display: flex;
+  gap: 20px;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+
+nav a.router-link-active {
+  border-bottom: 2px solid white;
+  font-weight: bold;
+}
+</style>

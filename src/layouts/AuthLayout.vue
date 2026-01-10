c7f76ccb8049fc1a659cc72bc12e190e5379cb3a
Redouane <redouane.takdjerad@gmail.com>
2026-01-10 19:42:13 +0100

    AuthLayout.vue component

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout-wrapper">
+    <div class="layout-card">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.layout-wrapper {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: #f4f6f8;
+  padding-top: 80px;
+}
+
+.layout-card {
+  background-color: #ffffff;
+  padding: 2rem;
+  border-radius: 12px;
+  width: 100%;
+  max-width: 420px;
+  box-shadow: 0 12px 30px rgba(0, 0, 0, 0.12);
+}
+</style>

4f807266c65c7d8344f4cd1433729ee03fd3e742
Redouane <redouane.takdjerad@gmail.com>
2026-01-10 19:40:33 +0100

    BaseButton.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
   import BaseInput from './components/commons/BaseInput.vue';
+  import BaseButton from './components/commons/BaseButton.vue';
 </script>
 
 <template>
- <BaseInput label="Firstname" type="text" id="firstname-input"/>
+  <form>
+    <BaseInput label="Firstname" type="text" id="firstname-input"/>
+    <BaseButton type="submit">Press button</BaseButton>
+  </form>
+
 </template>
 
 <style scoped></style>
---
+++ src/components/commons/BaseButton.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { ButtonHTMLAttributes } from 'vue';
+
+
+  interface Props{
+    type?: ButtonHTMLAttributes["type"]
+  }
+
+  const props = withDefaults(defineProps<Props>(),{
+    type: 'button'
+  })
+</script>
+
+<template>
+  <button :type="props.type" class="base-button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.base-button {
+  padding: 0.6rem 1rem;
+  background-color: #4f46e5;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  font-weight: 500;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.base-button:hover {
+  background-color: #4338ca;
+}
+
+.base-button:disabled {
+  background-color: #a5b4fc;
+  cursor: not-allowed;
+}
+</style>

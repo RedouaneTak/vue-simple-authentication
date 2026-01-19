67d0f295159826a20626fc0128a040c25b2549f4
Redouane <redouane.takdjerad@gmail.com>
2026-01-19 20:09:41 +0100

    getUserInfo api call

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,13 +1,17 @@
 import api from '@/api/axios'
-import type { LoginRequest, RegisterRequest } from '@/models/auth.model';
+import type { LoginRequest, RegisterRequest } from '@/models/auth.model'
 
-export const authenticate = async (credentials: LoginRequest) =>{
-  const response = await api.post('auth/authenticate',credentials);
-  return response.data;
+export const authenticate = async (credentials: LoginRequest) => {
+  const response = await api.post('auth/authenticate', credentials)
+  return response.data
 }
 
+export const register = async (userData: RegisterRequest) => {
+  const response = await api.post('auth/register', userData)
+  return response.data
+}
 
-export const register = async (userData:RegisterRequest) => {
-  const response = await api.post('auth/register', userData);
-  return response.data;
+export const getUserInfo = async () => {
+  const response = await api.post('auth/le')
+  return response.data
 }

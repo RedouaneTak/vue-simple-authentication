c11aa2e9884fdd1ad9c7c6ab07e46e411aebae4e
Redouane <redouane.takdjerad@gmail.com>
2026-01-23 14:34:16 +0100

    Router guard to redirect authenticated users trying to reach url /register and /login

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import LoginPage from '@/pages/LoginPage.vue'
 import RegisterPage from '@/pages/RegisterPage.vue'
+import { useAuth } from '@/stores/auth.store'
 
 const routes = [
-  { path: '/', redirect: 'home', meta: { requiresAuth: false }},
-  { path: '/home', name: 'home', component: HomePage, meta: { requiresAuth: false } },
+  { path: '/', redirect: 'home'},
+  { path: '/home', name: 'home', component: HomePage},
   { path: '/login', name: 'login', component: LoginPage, meta: { guestOnly: true } },
   { path: '/register', name: 'register', component: RegisterPage, meta: { guestOnly: true } },
 ]
@@ -13,6 +14,17 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to) => {
+  const store = useAuth()
+  const isAuthenticated = store.isAuthenticated
+
+  if (to.meta.guestOnly && isAuthenticated) {
+    return { name: 'home' }
+  }
+
+  return true
 })
 
 export default router

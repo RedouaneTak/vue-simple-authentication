0e88717c601fa3689c57fa43155f1ffa5c28b12b
Redouane <redouane.takdjerad@gmail.com>
2026-01-26 16:04:21 +0100

    HomePage css and paragraph to introduce the project

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Welcome to the Home Page!</h1>
-  <p> This is a simple Vue 3 application</p>
+  <main class="home-wrapper">
+    <div class="home-container">
+      <h1>Welcome to the Home Page!</h1>
+      <p>
+        This project is a personal portfolio application designed to showcase my skills in modern frontend development using Vue.js.
+        The interface communicates with a Spring Boot backend through a REST API, demonstrating my ability to build full-stack applications,
+        structure scalable components, and handle real-world data flows.
+      </p>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.home-wrapper {
+  min-height: calc(100vh - 70px); /* hauteur du header */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.home-container {
+  text-align: center;
+  padding: 20px;
+  max-width: 800px;
+  width: 100%;
+
+  transform: translateY(-80px);
+}
+
+h1 {
+  margin-bottom: 20px;
+  font-size: 2.2rem;
+}
+
+p {
+  line-height: 1.6;
+  font-size: 1.1rem;
+  color: #444;
+}
+</style>

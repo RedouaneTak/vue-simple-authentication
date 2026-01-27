0c1f7263576c6263bda01d509a66e633e20929be
Redouane <redouane.takdjerad@gmail.com>
2026-01-27 15:31:46 +0100

    UserPage for authenticated users

---
+++ src/pages/UserPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useAuth } from '@/stores/auth.store';
+import AuthLayout from '@/layouts/AuthLayout.vue';
+
+const store = useAuth()
+
+</script>
+
+<template>
+  <AuthLayout>
+    <h1>Me page working !</h1>
+    <h2 v-if="store.user">Hey {{ store.user.firstname }}</h2>
+  </AuthLayout>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import HomePage from '@/pages/HomePage.vue'
 import LoginPage from '@/pages/LoginPage.vue'
 import RegisterPage from '@/pages/RegisterPage.vue'
 import { useAuth } from '@/stores/auth.store'
+import UserPage from '@/pages/UserPage.vue'
 
 const routes = [
   { path: '/', redirect: 'home'},
   { path: '/home', name: 'home', component: HomePage},
   { path: '/login', name: 'login', component: LoginPage, meta: { guestOnly: true } },
   { path: '/register', name: 'register', component: RegisterPage, meta: { guestOnly: true } },
+  { path: '/me', name: 'me', component: UserPage}
 ]
 
 const router = createRouter({
